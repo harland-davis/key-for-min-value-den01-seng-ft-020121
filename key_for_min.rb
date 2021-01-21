7c5b0d289d2c75582dcd0511246cf4f8c0416be5
Kyle Davis <davis.h.kyle@gmail.com>
2021-01-21 20:30:14 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -4,10 +4,12 @@
 def key_for_min_value(name_hash)
 
 
-comparison = 1000
+lowest_value = 1000
 lowest_value_key = nil 
 name_hash.each do |key, value|
-  if value <= comparison
-    lowest_value_key = 
+  if value <= lowest_value
+    lowest_value = value 
+    
+    
 
 end
